3af2001a35bc728d1052b108d82876420962c7b0
Lia2406 <amaliakhoerina25@gmail.com>
2025-02-20 22:07:49 +0700

    komen.java

--- Constants.java
+++ Constants.java
@@ -8,3 +8,11 @@ public class Constants {
     CM_PER_INCH);
     }
     }
+
+//Outputnya : paper size in centimeters : 21.59 by 27.94
+//Menggunakan final double (constants) variabelnya adalah konstanta yang hanya bisa
+dipakai di metode main tidak bisa diakses diluar main/ class.
+//Public static final variabelnya bersifat konstanta global yang mana bisa di pakai di
+seluruh class. Dengan public konstanta ini dapat diakses di class lain dan apabila static
+maka berlaku di seluruh class(bukan hanya memungkinkan tapi bisa) tidak hanya untuk
+objek tertentu.
