9e682de8f37de7eb6cb5fa357670182154c1d9ff
Lia2406 <amaliakhoerina25@gmail.com>
2025-02-20 21:55:06 +0700

    Add files via upload

---
+++ StringManipulation.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class StringManipulation {
+   //memanipulasi isi string
+   public static void main(String[] args) {
+    Scanner scanner = new Scanner (System.in); //membaca input user/ keyboard
+    
+    System.out.println("masukan string pertama : ");
+    String A = scanner.nextLine(); //fungsi baca input angka di string
+    System.out.println("Masukkan String/kata kedua : ");
+    String B = scanner.nextLine();
+
+    int totalLength = A.length() + B.length(); //menghitung jumlah length
+    System.out.println(totalLength);
+
+    if (A.compareTo(B) > 0){ //membandingkan besarnya
+        System.out.println("Yes");
+    }else {
+        System.out.println("No");
+    }
+    
+    String capitalizedA = A.substring(0, 1).toUpperCase() + A.substring(1);
+    String capitalizedB = B.substring(0,1).toUpperCase() + B.substring(1);
+
+    System.out.println(capitalizedA + " " + capitalizedB);
+    scanner.close();
+   } 
+}
---
+++ datatype.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class datatype {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah test case: ");
+        if (!scanner.hasNextInt()) { 
+            // Cek apakah input pertama adalah angka
+            System.out.println("Input tidak valid. Harus berupa angka.");
+            //Menampilkan jumlah test case yang dimasukkan
+            return;
+        }
+        int T = scanner.nextInt(); 
+        scanner.nextLine(); // Membersihkan buffer
+
+        for (int i = 0; i < T; i++) { //loop sebanyak T kali untuk membaca angka n sebagai string
+            System.out.print("Masukkan angka: "); 
+            String n = scanner.nextLine();  // baca input angka dalam bentuk string
+
+            try {
+                long value = Long.parseLong(n);  //untuk mencoba ubah ke long
+                System.out.println(n + " can be fitted in:");
+
+                if (value >= Byte.MIN_VALUE && value <= Byte.MAX_VALUE) {
+                    System.out.println("* byte");
+                }
+                if (value >= Short.MIN_VALUE && value <= Short.MAX_VALUE) {
+                    System.out.println("* short");
+                }
+                if (value >= Integer.MIN_VALUE && value <= Integer.MAX_VALUE) {
+                    System.out.println("* int");
+                }
+                if (value >= Long.MIN_VALUE && value <= Long.MAX_VALUE) {
+                    System.out.println("* long");
+                }
+
+            } catch (NumberFormatException e) {
+                System.out.println(n + " cannot be stored in any primitive data type.");
+            }
+        }
+
+        scanner.close();
+    }
+}
