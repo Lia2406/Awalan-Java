5de60295cf478cf4d5d81932a7984a23f40d86de
Lia2406 <amaliakhoerina25@gmail.com>
2025-02-20 22:08:40 +0700

    komen.java

--- Constants2.java
+++ Constants2.java
@@ -7,3 +7,10 @@ public class Constants2 {
     CM_PER_INCH + " by " + paperHeight * CM_PER_INCH);
     }
     }
+
+//Output dari Constants2 : Paper size in centimeters : 21.59 by 27.94
+//yang ini menggunakan public static final
+//Public static final variabelnya bersifat konstanta global yang mana bisa di pakai di
+seluruh class. Dengan public konstanta ini dapat diakses di class lain dan apabila static
+maka berlaku di seluruh class(bukan hanya memungkinkan tapi bisa) tidak hanya untuk
+objek tertentu.
