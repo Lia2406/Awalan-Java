dbd773c9c875bfb34f95729d3cae7dec2ce9c2e4
Lia2406 <amaliakhoerina25@gmail.com>
2025-02-20 22:12:46 +0700

    Komen.java

--- ConvertDataType.java
+++ ConvertDataType.java
@@ -11,3 +11,12 @@ class ConvertDataType {
         System.out.println(b);
     }
 }
+
+//Outputnya 10 karena dikonversikan
+/*2. Yang berubah dari d ke b setelah casting
+Int,short,byte b tidak berubah karena masih dalam batas maksimal dan minimal tipe
+tersebut
+Dari yang awalnya double d yang di konversi ke float yang hanya berkurang presisi
+karena float lebih kecil dari double. Lalu float konversi ke long yang dibulatkan kebawah
+(casting selalu kebawah[float ke long] -&gt; truncation, kecuali pembulatan di math.round).
+lalu konversi dari long ke int ke shortke byte yang tidak berubah sama sekali*/
